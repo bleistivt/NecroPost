c8966441d7e8c203d0a7f56b7dcb5460e34bf3a9
bleistivt <bleistivt@users.noreply.github.com>
2015-09-12 09:41:20 +0200

    format

--- class.necropost.plugin.php
+++ class.necropost.plugin.php
@@ -17,7 +17,6 @@ class NecropostPlugin extends Gdn_Plugin {
     public function settingsController_necropost_create($sender) {
         $sender->permission('Garden.Settings.Manage');
         $sender->addSideMenu('settings/necropost');
-        $sender->setData('Title', T('Necropost Warning'));
 
         $conf = new ConfigurationModule($sender);
         $conf->initialize([
@@ -34,8 +33,11 @@ class NecropostPlugin extends Gdn_Plugin {
                 'Options' => ['MultiLine' => true]
             ]
         ]);
+        
+        $sender->title(t('Necropost Warning'));
         $conf->renderAll();
     }
+
 
     public function discussionController_render_before($sender) {
         if (!$sender->data('Discussion') || time() - strtotime($sender->data('Discussion')->DateLastComment) < c('Necropost.Days', 365) * 86400) {
@@ -51,6 +53,7 @@ class NecropostPlugin extends Gdn_Plugin {
             </script>'
         );
     }
+
 
     private function message() {
         return t(c('Necropost.Message', 'This discussion has been inactive for more than a year. Please comment only if you have something constructive to add.'));
